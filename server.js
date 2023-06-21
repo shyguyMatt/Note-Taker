5d51b4e42e7f7fdef565a057ead0f003e1288a9c
ShyguyMatt <mjherford@gmail.com>
2023-06-20 21:34:19 -0400

    fixed npm start

--- server.js
+++ server.js
@@ -14,11 +14,11 @@ app.use(express.urlencoded({ extended: true}));
 app.use(express.static('public'));
 
 app.get('/', (req, res) =>
-    res.sendFile(path.join(__dirname, '/index.html'))
+    res.sendFile(path.join(__dirname, '/public/index.html'))
 );
 
 app.get('/notes', (req, res) =>
-    res.sendFile(path.join(__dirname, '/notes.html'))
+    res.sendFile(path.join(__dirname, '/public/notes.html'))
 )
 
 app.get('/api/notes', (req, res) => {
@@ -35,6 +35,7 @@ app.post('/api/notes', (req, res) => {
         };
 
         readAndAppend(newNote, './db/db.json');
+        res.json('successfuly saved')
     } else {
         res.error('Error adding new note')
     }
