b53b0058d04f2e21f5a8c09f125f491b6a434432
ShyguyMatt <mjherford@gmail.com>
2023-06-20 21:37:48 -0400

    fixed port env

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const fs = require('fs')
 
 const { readFromFile, readAndAppend } = require('./helpers/fsUtils');
 
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 
 const app = express();
 
