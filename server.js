1398fe6557a0968f3a1119bc9da2dfb5b785d359
ShyguyMatt <mjherford@gmail.com>
2023-06-20 20:47:27 -0400

    changed public dir paths again

--- server.js
+++ server.js
@@ -14,11 +14,11 @@ app.use(express.urlencoded({ extended: true}));
 app.use(express.static('public'));
 
 app.get('/', (req, res) =>
-    res.sendFile(path.join(__dirname, 'index.html'))
+    res.sendFile(path.join(__dirname, '/index.html'))
 );
 
 app.get('/notes', (req, res) =>
-    res.sendFile(path.join(__dirname, 'notes.html'))
+    res.sendFile(path.join(__dirname, '/notes.html'))
 )
 
 app.get('/api/notes', (req, res) => {
