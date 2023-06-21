226bc71a3acd4c367548d6d046ca33781b75ff60
ShyguyMatt <mjherford@gmail.com>
2023-06-20 20:13:41 -0400

    committing to deploy to heroku, hope fixed path

--- server.js
+++ server.js
@@ -14,11 +14,11 @@ app.use(express.urlencoded({ extended: true}));
 app.use(express.static('public'));
 
 app.get('/', (req, res) =>
-    res.sendFile(path.join(__dirname, '/public/index.html'))
+    res.sendFile(path.join(__dirname, './public/index.html'))
 );
 
 app.get('/notes', (req, res) =>
-    res.sendFile(path.join(__dirname, '/public/notes.html'))
+    res.sendFile(path.join(__dirname, './public/notes.html'))
 )
 
 app.get('/api/notes', (req, res) => {
