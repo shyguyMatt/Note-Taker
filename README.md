4e06ed344b4bc80e04a60664a918081d341b6010
ShyguyMatt <mjherford@gmail.com>
2023-06-20 21:57:35 -0400

    added comments and README

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
+# Note taker app
+## Requirements
+write a backend app that connects with the prebuilt frontend to add functionality.
 
+there should be routes for the homepage "/",
+routes for the notes page "notes",
+and api routes to send and recieve data.
+
+## Screenshots
+![Screenshot 1](./screenshots/Capture1.PNG)
+![Screenshot 2](./screenshots/Capture2.PNG)
+![Screenshot 2](./screenshots/Capture3.PNG)
+
+
+## Links
+https://github.com/shyguyMatt/Note-Taker
+https://shyguymatt-note-taker-ac7735de2995.herokuapp.com/
--- server.js
+++ server.js
@@ -2,29 +2,42 @@ const express = require('express');
 const path = require('path');
 const fs = require('fs')
 
+
+// importing helper functions
 const { readFromFile, readAndAppend } = require('./helpers/fsUtils');
 
+// port for heroku and localhost
 const PORT = process.env.PORT || 3001;
 
 const app = express();
 
+// middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: true}));
 
 app.use(express.static('public'));
 
+// route for homepage
 app.get('/', (req, res) =>
     res.sendFile(path.join(__dirname, '/public/index.html'))
 );
 
+// route for notes page
 app.get('/notes', (req, res) =>
     res.sendFile(path.join(__dirname, '/public/notes.html'))
 )
 
+// catch all route redirects to homepage
+app.get('*', (req, res) => 
+    res.sendFile(path.join(__dirname, 'public/index.html'))
+)
+
+// api returns db.json
 app.get('/api/notes', (req, res) => {
     readFromFile('./db/db.json').then((data) => res.json(JSON.parse(data)))
 });
 
+// api writes to db.json
 app.post('/api/notes', (req, res) => {
     const { title, text } = req.body;
 
@@ -41,6 +54,7 @@ app.post('/api/notes', (req, res) => {
     }
 })
 
+// listening port
 app.listen(PORT, () =>
     console.log(`App listening at http://localhost:${PORT}`)
 );
